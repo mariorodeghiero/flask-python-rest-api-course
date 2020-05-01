501cec9d21e510c7be94c0a4270cb4c3336a3a77
Mario Rodeghiero <mario.rodeghiero@gmail.com>
2020-04-30 22:44:33 -0300

    [post method][s]: creat post method add new hotel  - flask-course #1

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,23 +1,26 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 hotels = [
     {
         'hotel_id': 'alpha',
         'name': 'alpha Hotel',
         'stars': 4.3,
-        'price': 420
+        'price': 420,
+        'city': 'Floripa'
     },
     {
         'hotel_id': 'bravo',
         'name': 'Bravo Hotel',
         'stars': 5,
-        'price': 220
+        'price': 220,
+        'city': 'Floripa'
     },
     {
         'hotel_id': 'charlie',
         'name': 'Charlie Hotel',
         'stars': 3,
-        'price': 550
+        'price': 550,
+        'city': 'Floripa'
     }
 ]
 
@@ -33,7 +36,24 @@ class Hotel(Resource):
         return { 'message': 'Hotel not found'}, 404
 
     def post(self, hotel_id):
-        pass
+        args = reqparse.RequestParser()
+        args.add_argument('name')
+        args.add_argument('stars')
+        args.add_argument('price')
+        args.add_argument('city')
+
+        data = args.parse_args()
+
+        new_hotel = {
+            'hotel_id': hotel_id,
+            'name': data['name'],
+            'stars': data['stars'],
+            'price': data['price'],
+            'city': data['city']
+        }
+
+        hotels.append(new_hotel)
+        return new_hotel, 200
 
     def put(self, hotel_id):
         pass
