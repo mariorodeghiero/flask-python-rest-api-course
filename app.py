3dc115ad4203f9d43fa66f941e22101cb2cbaffb
Mario Rodeghiero <mario.rodeghiero@gmail.com>
2020-05-01 19:17:17 -0300

    [templates][s]: add templates and home route flask-course

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Api
 from resources.hotel import Hotels, Hotel
 
 app = Flask(__name__)
 api = Api(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 api.add_resource(Hotels, '/hotels')
 api.add_resource(Hotel, '/hotels/<string:hotel_id>')
