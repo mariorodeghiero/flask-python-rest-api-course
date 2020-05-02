e6c8befa694cbcb8509b5b4f619103cabcd321ca
Mario Rodeghiero <mario.rodeghiero@gmail.com>
2020-05-02 20:36:10 -0300

    [methods][s]: add put and post methods flask-course #1

--- resources/hotel.py
+++ resources/hotel.py
@@ -29,34 +29,45 @@ class Hotels(Resource):
        return { 'hotels': hotels}
 
 class Hotel(Resource):
-    def get(self, hotel_id):
+    args = reqparse.RequestParser()
+    args.add_argument('name')
+    args.add_argument('stars')
+    args.add_argument('price')
+    args.add_argument('city')
+
+    def find_hotel(hotel_id):
         for hotel in hotels:
             if hotel['hotel_id'] == hotel_id:
                 return hotel
+        return None
+
+    def get(self, hotel_id):
+
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            return hotel
         return { 'message': 'Hotel not found'}, 404
 
     def post(self, hotel_id):
-        args = reqparse.RequestParser()
-        args.add_argument('name')
-        args.add_argument('stars')
-        args.add_argument('price')
-        args.add_argument('city')
 
-        data = args.parse_args()
+        data = Hotel.args.parse_args()
 
-        new_hotel = {
-            'hotel_id': hotel_id,
-            'name': data['name'],
-            'stars': data['stars'],
-            'price': data['price'],
-            'city': data['city']
-        }
+        new_hotel = { 'hotel_id': hotel_id, **data}
 
         hotels.append(new_hotel)
         return new_hotel, 200
 
     def put(self, hotel_id):
-        pass
+
+        data = Hotel.args.parse_args()
+        new_hotel = { 'hotel_id': hotel_id, **data}
+
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            hotel.update(new_hotel)
+            return new_hotel, 200
+        hotels.append(new_hotel)
+        return new_hotel, 201
 
     def delete(self, hotel_id):
         pass
